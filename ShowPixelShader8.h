f2db8b4d82841a9059311922604f830978b69f98
DJCEL <djcel@hotmail.fr>
2025-06-29 18:49:36 +0200

    Update ShowPixelShader8.h

--- ShowPixelShader8.h
+++ ShowPixelShader8.h
@@ -87,6 +87,7 @@ private:
 	HRESULT Rendering_D3D11(ID3D11Device* pDevice, ID3D11ShaderResourceView* pTextureView, TVertex8* pVertices);
 	HRESULT Create_PixelShader_D3D11(ID3D11Device* pDevice);
 	HRESULT Create_PixelShaderFromCSOFile_D3D11(ID3D11Device* pDevice, const WCHAR* pShaderFilepath);
+	HRESULT Create_PixelShaderFromHLSLFile_D3D11(ID3D11Device* pDevice, const WCHAR* pShaderFilepath);
 	HRESULT Create_PixelShaderFromHeaderFile_D3D11(ID3D11Device* pDevice);
 	HRESULT Create_PixelShaderFromResourceCSOFile_D3D11(ID3D11Device* pDevice, const WCHAR* resourceType, const WCHAR* resourceName);
 	HRESULT Create_VertexBufferDynamic_D3D11(ID3D11Device* pDevice);
