4f4ebb3235446af0ccc9c4488146fd657f56bdd5
DJCEL <16272026+DJCEL@users.noreply.github.com>
2025-07-11 23:47:56 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ HRESULT VDJ_API DllGetClassObject(const GUID &rclsid,const GUID &riid,void** ppO
 {
 	// This is the standard DLL loader for COM object.
 
-	if (memcmp(&rclsid, &CLSID_VdjPlugin8, sizeof(GUID)) == 0 && memcmp(&riid, &IID_IVdjPluginVideoFx8, sizeof(GUID)) == 0)
+	if (memcmp(&rclsid, &CLSID_VdjPlugin8, sizeof(GUID))==0 && memcmp(&riid, &IID_IVdjPluginVideoFx8, sizeof(GUID))==0)
 	{
 		*ppObject = new CShowPixelShader8();
 	}
