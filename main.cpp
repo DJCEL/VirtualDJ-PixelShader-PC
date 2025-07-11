a99b592e13e7c4f900ebc73bcdb4b68024ac4715
DJCEL <16272026+DJCEL@users.noreply.github.com>
2025-07-11 23:46:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 HRESULT VDJ_API DllGetClassObject(const GUID &rclsid,const GUID &riid,void** ppObject)
 {
 	// This is the standard DLL loader for COM object.
-	// You don't need to change anything in this function.
 
 	if (memcmp(&rclsid, &CLSID_VdjPlugin8, sizeof(GUID)) == 0 && memcmp(&riid, &IID_IVdjPluginVideoFx8, sizeof(GUID)) == 0)
 	{
